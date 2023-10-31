b97f165629e28e8e8a0ba940d3090a7d0b3b091b
elzurdok <barrionuevotomas11@gmail.com>
2023-10-31 18:10:04 -0300

    Update1.4

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,16 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    const elementosCarousel = document.querySelectorAll('.carousel');
+    M.Carousel.init(elementosCarousel, {
+        duration: 150,
+        dist: -80,
+        shift: 5,
+        padding: 5,
+        numVisible: 3,
+        indicators: true,
+        noWrap: false
+    });
+});
